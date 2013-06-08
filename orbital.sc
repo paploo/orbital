a73fc8d5abf53d5b5c102dfbb180c5b453cc7823
Jeff Reinecke <jeff@paploo.net>
2013-06-07 21:42:54 -0700

    Steppable: Added analysis hook to runloop.

--- orbital.sc
+++ orbital.sc
@@ -14,14 +14,14 @@ object orbital {
 	rocket.period                             //> res1: Double = 2109.8459440561614
 	
 	
-	val last = rocket.stepsWhile(_.state.t < rocket.period)(0.01).last
-                                                  //> last  : net.paploo.orbital.Rocket = Rocket(State(2109.8399999996323,PhysVec(
-                                                  //| 700126.5417299438,-699.4898838692047,0.0),PhysVec(2.191310137269498,2299.797
-                                                  //| 955544316,0.0)), 10.0, Kerbin)
-	last.state.t                              //> res2: Double = 2109.8399999996323
-	last.state                                //> res3: net.paploo.orbital.State = State(2109.8399999996323,PhysVec(700126.541
-                                                  //| 7299438,-699.4898838692047,0.0),PhysVec(2.191310137269498,2299.797955544316,
-                                                  //| 0.0))
-	last.apses                                //> res4: (Double, Double) = (700126.8749720006,771562.7551663277)
-	last.period                               //> res5: Double = 2110.442214842026
+	val last = rocket.runWhile(_.state.t < rocket.period)(0.0001)
+                                                  //> last  : net.paploo.orbital.Rocket = Rocket(State(2109.8458996826425,PhysVec(
+                                                  //| 700001.268745336,-6.959514635671901,0.0),PhysVec(0.02180847500915307,2299.99
+                                                  //| 79904408033,0.0)), 10.0, Kerbin)
+	last.state.t                              //> res2: Double = 2109.8458996826425
+	last.state                                //> res3: net.paploo.orbital.State = State(2109.8458996826425,PhysVec(700001.268
+                                                  //| 745336,-6.959514635671901,0.0),PhysVec(0.02180847500915307,2299.997990440803
+                                                  //| 3,0.0))
+	last.apses                                //> res4: (Double, Double) = (700001.2687783306,771413.919850391)
+	last.period                               //> res5: Double = 2109.851907688307
 }
--- src/main/scala/net/paploo/orbital/Steppable.scala
+++ src/main/scala/net/paploo/orbital/Steppable.scala
@@ -9,7 +9,10 @@ import scala.annotation.tailrec
 trait Steppable[+T <: Steppable[T]] {
   this: T =>
 
-  /** The base step method signature; this must be overriden */
+  /** Type for the AnalyzerHook function. **/
+  type AnalyzerHook = (T, T) => Boolean
+
+  /** The base step method signature; this must be overridden */
   def step(deltaT: Double): T
 
   /** Produces a stream of steps. */
@@ -23,7 +26,7 @@ trait Steppable[+T <: Steppable[T]] {
    *
    *  Calling stepsWhile(cond)(deltaT).last runs the heap out quickly, so it
    *  is better to use runWhile if you don't need to work with intermediate
-   *  values.
+   *  values, or runAnalysisWhile if you need to work with intermediate values.
    *
    */
   def stepsWhile(cond: T => Boolean)(deltaT: Double): Stream[T] = this #:: {
@@ -35,10 +38,35 @@ trait Steppable[+T <: Steppable[T]] {
    * Runs while the condition is true, returning the last evaluation for which
    * it is true.
    */
-  def runWhile(cond: T => Boolean)(deltaT: Double): T = {
+  def runWhile(cond: T => Boolean)(deltaT: Double): T =
+    runAnalysisWhile((_, nextFrame) => cond(nextFrame))(deltaT)
+
+  /**
+   * Runs calling the analyzerHook at each step, giving both the current and next frame,
+   * and stopping when the analyzerHook returns false. Returns the last frame for
+   * which the analyzerHook returned true.
+   *
+   * Implementation of runWhile can be achieved by passing
+   * {{{
+   * (_, nextFrame) => cond(nextFrame)
+   * }}}
+   * as the hook; this will return the last step for which the condition was true.
+   *
+   * If, for example, you wanted the return to be the first frame for which it is
+   * not true, you could instead give
+   * {{{
+   * (frame, _) => cond(frame)
+   * }}}
+   *
+   * The analyzerHook can be an instance method on an object. This allows
+   * for simple things (e.g. writing to a log), or more complex
+   * things (e.g. event detection and notification, or on a mutable object,
+   * collection of statistics.
+   */
+  def runAnalysisWhile(analyzerHook: AnalyzerHook)(deltaT: Double): T = {
     var frame = this
     var nextFrame = frame.step(deltaT)
-    while (cond(nextFrame)) {
+    while (analyzerHook(frame, nextFrame)) {
       frame = nextFrame
       nextFrame = frame.step(deltaT)
     }
